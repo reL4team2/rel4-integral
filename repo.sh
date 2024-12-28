fe6d182a4ccffdeced8835016b1a755306648100
jackhu <jackhu.521.rose@gmail.com>
2024-12-28 13:45:24 +0800

    modify branch name

--- repo.sh
+++ repo.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-urls=("git@github.com:rel4team/sel4_common.git,sel4_common"
-  "git@github.com:rel4team/sel4_task.git,sel4_task"
-  "git@github.com:rel4team/sel4_ipc.git,sel4_ipc"
-  "git@github.com:rel4team/sel4_vspace.git,sel4_vspace"
-  "git@github.com:rel4team/sel4_cspace.git,sel4_cspace"
-  "git@github.com:rel4team/rel4_kernel.git,kernel"
-  "git@github.com:rel4team/driver-collect.git,driver-collect"
-  "git@github.com:rel4team/serial-impl-pl011.git,serial-impl/pl011"
-  "git@github.com:rel4team/serial-impl-sbi.git,serial-impl/sbi"
-  "git@github.com:rel4team/serial-frame.git,serial-frame"
+urls=("git@github.com:reL4team2/sel4_common.git,sel4_common"
+  "git@github.com:reL4team2/sel4_task.git,sel4_task"
+  "git@github.com:reL4team2/sel4_ipc.git,sel4_ipc"
+  "git@github.com:reL4team2/sel4_vspace.git,sel4_vspace"
+  "git@github.com:reL4team2/sel4_cspace.git,sel4_cspace"
+  "git@github.com:reL4team2/rel4_kernel.git,kernel"
+  "git@github.com:reL4team2/driver-collect.git,driver-collect"
+  "git@github.com:reL4team2/serial-impl-pl011.git,serial-impl/pl011"
+  "git@github.com:reL4team2/serial-impl-sbi.git,serial-impl/sbi"
+  "git@github.com:reL4team2/serial-frame.git,serial-frame"
 )
-branch=mi_dev
+branch=master
 
 for str in ${urls[@]}; do
   IFS=',' read -r -a inner_array <<<"$str"
